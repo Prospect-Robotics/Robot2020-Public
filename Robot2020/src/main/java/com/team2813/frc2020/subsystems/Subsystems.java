59845f351fc332291c4f4a85600a3131698f0a32
Siddharth Banerjee <46699142+sbanerjee13@users.noreply.github.com>
2020-01-05 12:47:59 -0800

    merge drive/lib

---
+++ Robot2020/src/main/java/com/team2813/frc2020/subsystems/Subsystems.java
@@ -0,0 +1,45 @@
+package com.team2813.frc2020.subsystems;
+
+import com.team2813.frc2019.Robot.RobotMode;
+import com.team2813.frc2019.loops.Loop;
+import com.team2813.frc2019.loops.Looper;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Stores a list of all subsystems and initializes them and
+ * runs a loop to put things on the SmartDashboard.
+ */
+public class Subsystems {
+
+	public static List<Subsystem> allSubsystems;
+
+	public static Drive DRIVE;
+	public static GroundIntake GROUND_INTAKE;
+	public static MainIntake MAIN_INTAKE;
+	public static final Looper LOOPER = new Looper(RobotMode.DISABLED); //FIXME put looper somewhere else
+
+	private static class SmartDashboardLoop implements Loop{
+		int currentSubsystem = 0;
+
+		@Override
+		public void onAnyLoop(double timestamp){
+			if(allSubsystems.size() == 0) return;
+			if(currentSubsystem >= allSubsystems.size()) currentSubsystem = 0;
+			allSubsystems.get(currentSubsystem).outputTelemetry();
+			currentSubsystem++;
+		}
+	};
+
+	public static void initializeSubsystems() {
+		DRIVE = new Drive();
+		GROUND_INTAKE = new GroundIntake();
+		MAIN_INTAKE = new MainIntake();
+		allSubsystems = Collections.unmodifiableList(Arrays.asList(
+				DRIVE, GROUND_INTAKE, MAIN_INTAKE
+		));
+		LOOPER.addLoop(new SmartDashboardLoop());
+	}
+}
