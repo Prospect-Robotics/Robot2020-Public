f7d0facd61596d9c1478950bc8f52ac70724f242
abhineetpal <aip_embox@thepal.org>
2020-01-10 21:52:37 -0800

    created robot test class

---
+++ Robot2020/src/main/java/com/team2813/lib/util/RobotTest.java
@@ -0,0 +1,10 @@
+package com.team2813.lib.util;
+
+import com.team2813.frc2020.actions.SeriesAction;
+
+public class RobotTest {
+    public void TestInit(){
+        SeriesAction test = new SeriesAction();
+        
+    }
+}
