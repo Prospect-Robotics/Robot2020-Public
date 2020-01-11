190debd2e14741042628ffd0029066a28a87f61a
abhineetpal <aip_embox@thepal.org>
2020-01-11 12:15:15 -0800

    made function to test driving

---
+++ Robot2020/src/main/java/com/team2813/frc2020/util/RobotTest.java
@@ -0,0 +1,20 @@
+package com.team2813.frc2020.util;
+
+import com.team2813.frc2020.actions.FunctionAction;
+import com.team2813.frc2020.actions.SeriesAction;
+import com.team2813.frc2020.subsystems.Subsystems;
+
+/**
+ * @Author Abhineet Pal
+ */
+public class RobotTest {
+    public void run() {
+        SeriesAction action = new SeriesAction(
+                new FunctionAction(Subsystems.DRIVE::driveForwardTest, true),
+                new FunctionAction(Subsystems.DRIVE::driveBackwardsTest, true),
+                new FunctionAction(Subsystems.DRIVE::driveRightTest, true),
+                new FunctionAction(Subsystems.DRIVE::driveLeftTest, true)
+        );
+        Subsystems.LOOPER.addAction(action);
+    }
+}
