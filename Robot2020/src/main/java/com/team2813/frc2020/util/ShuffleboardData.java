a30d25e625698a4094972c4ef5edb0e0d2c450e0
Siddharth Banerjee <46699142+sbanerjee13@users.noreply.github.com>
2020-01-17 15:22:25 -0800

    Update ShuffleboardData.java

--- Robot2020/src/main/java/com/team2813/frc2020/util/ShuffleboardData.java
+++ Robot2020/src/main/java/com/team2813/frc2020/util/ShuffleboardData.java
@@ -1,14 +1,14 @@
 package com.team2813.frc2020.util;
 
-import com.team2813.frc2020.subsystems.Drive;
+import com.team2813.frc2020.subsystems.DriveTalon;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ShuffleboardData {
     // define your smartdashboard entries here
     public static SendableChooser<AutonomousPath> pathChooser = new SendableChooser<>();
-    public static SendableChooser<Drive.DriveMode> driveModeChooser = new SendableChooser<>();
-    public static SendableChooser<Drive.TeleopDriveType> teleopDriveTypeChooser = new SendableChooser<>();
+    public static SendableChooser<DriveTalon.DriveMode> driveModeChooser = new SendableChooser<>();
+    public static SendableChooser<DriveTalon.TeleopDriveType> teleopDriveTypeChooser = new SendableChooser<>();
 
     public static void init() {
         SmartDashboard.putData("Auto Path", pathChooser);
