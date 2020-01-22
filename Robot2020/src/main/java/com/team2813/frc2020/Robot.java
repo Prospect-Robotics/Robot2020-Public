6e42e36fd103d9782b09a14645cbad7d43ba7159
abhineetpal <aip_embox@thepal.org>
2020-01-21 16:49:28 -0800

    more updates

--- Robot2020/src/main/java/com/team2813/frc2020/Robot.java
+++ Robot2020/src/main/java/com/team2813/frc2020/Robot.java
@@ -21,6 +21,7 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import java.io.IOException;
@@ -115,6 +116,7 @@ public class Robot extends TimedRobot {
     @Override
     public void autonomousInit() {
         isAuto = true;
+        DRIVE.robotPosition = new Pose2d();
         try {
             CrashTracker.logAutoInit();
 //            Compressor compressor = new Compressor(); // FIXME: 11/02/2019 this shouldn't need to be here
