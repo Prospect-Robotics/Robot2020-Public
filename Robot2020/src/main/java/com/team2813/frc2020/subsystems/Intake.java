d22ec24d70604f409b4c469b77e4cbd155bd762c
Siddharth Banerjee <46699142+sbanerjee13@users.noreply.github.com>
2020-01-24 15:36:23 -0800

    intake and subsystemscontrolconfig

--- Robot2020/src/main/java/com/team2813/frc2020/subsystems/Intake.java
+++ Robot2020/src/main/java/com/team2813/frc2020/subsystems/Intake.java
@@ -1,9 +1,17 @@
 package com.team2813.frc2020.subsystems;
 
+import com.team2813.lib.config.MotorConfigs;
+import com.team2813.lib.controls.Button;
 import com.team2813.lib.motors.SparkMaxWrapper;
-import com.team2813.lib.motors.TalonWrapper;
-        
+import com.team2813.lib.motors.VictorWrapper;
+
 public class Intake extends Subsystem {
+
+    private static final SparkMaxWrapper MOTOR = MotorConfigs.sparks.get("intake");
+    private static final double ROLLER_SPEED = .8; // # from OffSeasonBot2019 -- is this the right value?
+    private static final Button INTAKE_BUTTON = SubsystemControlsConfig.getIntakeButton();
+    private static double demand;
+
 
     @Override
     public void outputTelemetry() {
@@ -12,7 +20,7 @@ public class Intake extends Subsystem {
 
     @Override
     public void teleopControls() {
-
+        INTAKE_BUTTON.whenPressedReleased(() -> demand = ROLLER_SPEED, () -> demand = 0.0);
     }
 
     @Override
--- Robot2020/src/main/java/com/team2813/frc2020/subsystems/SubsystemControlsConfig.java
+++ Robot2020/src/main/java/com/team2813/frc2020/subsystems/SubsystemControlsConfig.java
@@ -19,11 +19,16 @@ class SubsystemControlsConfig {
 	private static Controller driveJoystick = new Controller(0);
 	private static Button pivotButton = driveJoystick.button(1);
 	private static Button autoButton = driveJoystick.button(2);
+	private static Button intakeButton = driveJoystick.button(3);
 	private static Axis driveX = driveJoystick.axis(0);
 	private static Axis driveY = driveJoystick.axis(3);
 	private static Axis driveSteer = driveJoystick.axis(0);
 	private static Axis driveForward = driveJoystick.axis(3);
 	private static Axis driveReverse = driveJoystick.axis(2);
+
+	static Button getIntakeButton() {
+		return intakeButton;
+	}
 
 	static Button getPivotButton() {
 		return pivotButton;
