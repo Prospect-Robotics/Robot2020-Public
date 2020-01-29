de1f2205f1bcaced8170640d7678697d69e67d90
Grady Whelan <grady.whelan@gmail.com>
2020-01-29 10:58:32 -0800

    Update RotateTrajectory.java

--- Robot2020/src/main/java/com/team2813/lib/auto/RotateTrajectory.java
+++ Robot2020/src/main/java/com/team2813/lib/auto/RotateTrajectory.java
@@ -3,7 +3,10 @@ package com.team2813.lib.auto;
 import com.team2813.frc2020.subsystems.Subsystems;
 import com.team2813.lib.ctre.PigeonWrapper;
 import com.team2813.lib.drive.DriveDemand;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
+
+import java.util.*;
 
 public class RotateTrajectory implements AutoTrajectory {
     private boolean reversed;
@@ -13,28 +16,35 @@ public class RotateTrajectory implements AutoTrajectory {
     private DriveDemand driveDemand;
     private PigeonWrapper pigeon = Subsystems.DRIVE.getPigeon();
 
+    private Trajectory trajectory;
+
 
 	// TODO method for determining how far robot has turned cannot be in constructor
 	// because this constructor is run before robot initialization is complete,
 	// not while the routine is being executed.
 
     public RotateTrajectory(double degrees, boolean reversed){
-        this.reversed = reversed;
-        maxVelocity = Subsystems.DRIVE.getMaxVelocity();
-        driveDemand = Subsystems.DRIVE.getDriveDemand();
-        originalHeading = pigeon.getHeading();
-        if(!reversed){
-            while(currentHeading != (originalHeading + degrees)){
-                driveDemand = new DriveDemand(maxVelocity / 2, -maxVelocity / 2);
-                currentHeading = pigeon.getHeading();
-            }
-        }
-        else{
-            while(currentHeading != (originalHeading - degrees)){
-                driveDemand = new DriveDemand(-maxVelocity / 2, maxVelocity / 2);
-                currentHeading = pigeon.getHeading();
-            }
-        }
+        Trajectory.State state = new Trajectory.State();
+        ArrayList<Trajectory.State> states = new ArrayList<>();
+        Pose2d pose = new Pose2d();
+        trajectory = new Trajectory(states);
+
+//        this.reversed = reversed;
+//        maxVelocity = Subsystems.DRIVE.getMaxVelocity();
+//        driveDemand = Subsystems.DRIVE.getDriveDemand();
+//        originalHeading = pigeon.getHeading();
+//        if(!reversed){
+//            while(currentHeading != (originalHeading + degrees)){
+//                driveDemand = new DriveDemand(maxVelocity / 2, -maxVelocity / 2);
+//                currentHeading = pigeon.getHeading();
+//            }
+//        }
+//        else{
+//            while(currentHeading != (originalHeading - degrees)){
+//                driveDemand = new DriveDemand(-maxVelocity / 2, maxVelocity / 2);
+//                currentHeading = pigeon.getHeading();
+//            }
+//        }
     }
 
     @Override
