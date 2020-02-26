c49b3a60a5f3ecbfa1a054df7f46caef622f75d0
saanvi25 <55930332+saanvi25@users.noreply.github.com>
2020-02-25 17:13:33 -0800

    added names to auto routine actions

--- Robot2020/src/main/java/com/team2813/frc2020/auto/AutoRoutineActions.java
+++ Robot2020/src/main/java/com/team2813/frc2020/auto/AutoRoutineActions.java
@@ -8,38 +8,40 @@ import com.team2813.lib.actions.WaitAction;
 import com.team2813.lib.auto.RamseteTrajectory;
 
 public enum AutoRoutineActions {
-    FIVE_BALL_ENEMY(
+    FIVE_BALL_ENEMY("5 Ball Enemy",
             new SeriesAction(
                     new AutoAimAction()/* shoot ball, intake, shoot ball*/
     ), AutoRoutineTrajectories.FIVE_BALL_ENEMY),
-    TEST_ROUTINE(
+    TEST_ROUTINE("Test",
             new SeriesAction(new WaitAction(1)
             ),AutoRoutineTrajectories.TEST_ROUTINE),
-    THREE_BALL(
+    THREE_BALL("3-ball",
             new SeriesAction(new WaitAction(1)
             ),AutoRoutineTrajectories.THREE_BALL),
-    SIX_BALL(
+    SIX_BALL("6-ball",
             new SeriesAction(new WaitAction(1)
             ),AutoRoutineTrajectories.SIX_BALL),
-    EIGHT_BALL(
+    EIGHT_BALL("8-ball",
             new SeriesAction(new WaitAction(1)
             ),AutoRoutineTrajectories.EIGHT_BALL),
-    FIVE_BALL_TWO(
+    FIVE_BALL_TWO("5-ball 2",
             new SeriesAction(new WaitAction(1)
             ),AutoRoutineTrajectories.FIVE_BALL_TWO),
-    TEST_REVERSE(
+    TEST_REVERSE("test reverse",
             new SeriesAction(new WaitAction(1)
             ),AutoRoutineTrajectories.TEST_REVERSE),
-    SERIES_TEST(
+    SERIES_TEST("Auto Aim Test",
             new SeriesAction(
                     new AutoAimAction()
             ),AutoRoutineTrajectories.SERIES_TEST);
     private Action action;
     private RamseteTrajectory trajectory;
+    public String name;
 
-    AutoRoutineActions(Action action, AutoRoutineTrajectories trajectory) {
+    AutoRoutineActions(String name, Action action, AutoRoutineTrajectories trajectory) {
         this.action = action;
         this.trajectory = trajectory.getTrajectory();
+        this.name = name;
     }
 
     Action getAction() {
