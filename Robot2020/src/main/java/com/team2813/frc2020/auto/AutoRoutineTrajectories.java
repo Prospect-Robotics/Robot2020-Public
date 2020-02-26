adb572ed7515d93e7345043fccb4df2f573b5dcc
abhineetpal <aip_embox@thepal.org>
2020-02-25 16:54:31 -0800

    filled in AutoRoutineTrajectories

--- Robot2020/src/main/java/com/team2813/frc2020/auto/AutoRoutineTrajectories.java
+++ Robot2020/src/main/java/com/team2813/frc2020/auto/AutoRoutineTrajectories.java
@@ -1,13 +1,66 @@
 package com.team2813.frc2020.auto;
 
-import com.team2813.lib.auto.AutoTrajectory;
-import com.team2813.lib.auto.RamseteTrajectory;
+import com.team2813.lib.actions.SeriesAction;
+import com.team2813.lib.actions.WaitAction;
+import com.team2813.lib.auto.*;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 
 import java.util.List;
 
 public enum AutoRoutineTrajectories {
-    FIE;
+    FIVE_BALL_ENEMY(List.of(
+            new PauseTrajectory(1, 0), // shoot 3 balls
+            new GeneratedTrajectory("2-ball", true, 1),
+            new PauseTrajectory(1, 2), // intake 2 balls, turn intake off
+            new GeneratedTrajectory("go back", false, 3),
+            new GeneratedTrajectory("return", false, 4),
+            new PauseTrajectory(1, 5) // shoot 2 balls
+    )),
+    TEST_ROUTINE(List.of(
+            new PauseTrajectory(0.5, 0),
+            new GeneratedTrajectory("test", false, 1),
+            new RotateTrajectory(180, 2),
+            new GeneratedTrajectory("another test path", false, 3)
+    )),
+    THREE_BALL(List.of(
+            new PauseTrajectory(1, 0), //shoot 3-balls
+            new GeneratedTrajectory("3-ball prepare", false, 1),
+            new PauseTrajectory(1, 2), //turn intake on
+            new GeneratedTrajectory("3-ball", true, 3), //intake 3 balls
+            new GeneratedTrajectory("2-ball 2", true, 4), //intake 2 balls
+            new PauseTrajectory(1, 5) //turn intake off
+    )),
+    SIX_BALL(List.of(
+            new PauseTrajectory(1, 0), // shoot 3 ball turn intake on
+            new GeneratedTrajectory("3-ball trench", true, 1), // intake 3 balls
+            new PauseTrajectory(1, 2), //turn intake off
+            new GeneratedTrajectory("back 3-ball", false, 3),
+            new PauseTrajectory(1, 4) // shoot 3 balls
+    )),
+    EIGHT_BALL(List.of(
+            new GeneratedTrajectory("back trench", true, 0),
+            new PauseTrajectory(1, 1), // turn intake on
+            new GeneratedTrajectory("2-ball shield generator", true, 2), // intake 2 balls
+            new GeneratedTrajectory("3-ball 2", true, 3), // intake 3 balls
+            new PauseTrajectory(1, 4), // turn intake off
+            new GeneratedTrajectory("return 8-ball", false, 5),
+            new PauseTrajectory(1, 6) // shoot 5 balls
+    )),
+    FIVE_BALL_TWO(List.of(
+            new PauseTrajectory(1, 0), //turn intake on
+            new GeneratedTrajectory("2-ball initiation line", true, 1), //intake 2 balls
+            new RotateTrajectory(180, 2),
+            new PauseTrajectory(1, 3), //turn intake off, shoot 5 balls
+            new GeneratedTrajectory("general zone", false, 4)
+    )),
+    TEST_REVERSE(List.of(
+            new GeneratedTrajectory("test reverse", true, 0)
+    )),
+    SERIES_TEST(List.of(
+            new PauseTrajectory(150, 0)
+    ));
+
+
 
     private RamseteTrajectory trajectory;
 
